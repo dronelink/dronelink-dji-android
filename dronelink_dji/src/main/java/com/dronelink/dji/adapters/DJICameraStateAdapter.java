9ec9dd5bfa29ae35113c176c5bb3028433960220
Nicolas Roberts <nicolas@dronelink.com>
2023-07-05 18:03:31 -0700

    remove unnecessary check for 0 step

--- dronelink_dji/src/main/java/com/dronelink/dji/adapters/DJICameraStateAdapter.java
+++ dronelink_dji/src/main/java/com/dronelink/dji/adapters/DJICameraStateAdapter.java
@@ -359,8 +359,7 @@ class DJICameraStateAdapter implements CameraStateAdapter {
     public PercentZoomSpec getZoomSpec() {
         final SettingsDefinitions.HybridZoomSpec spec = this.hybridZoomSpec;
         final Double zoomValue = this.zoomValue;
-        //Some cameras return true for isPercentZoomSupported but don't support zoom. The spec focal length is 0 when that is the case, so we can use that to check.
-        if(!isPercentZoomSupported() || spec == null || zoomValue == null || spec.getFocalLengthStep() == 0) {
+        if(!isPercentZoomSupported() || spec == null || zoomValue == null) {
             return null;
         }
         try {
